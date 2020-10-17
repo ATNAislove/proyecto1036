2491f6adab1654f77a029fabf0fa54322f6ab4e0
ATNAislove <al375915@uji.es>
2020-10-17 12:23:27 +0200

    database

--- includes/pdo_postgres.php
+++ includes/pdo_postgres.php
@@ -1,13 +1,13 @@
 <?php
 
 /** The name of the database  */
-define('DB_NAME', '');
+define('DB_NAME', 'al375915_al361883_ei1036_42');
 
-/** Fatabase username */
-define('DB_USER', '');
+/** Database username */
+define('DB_USER', 'al375915');
 
 /** Database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'cubsuelo');
 
 /** Database hostname */
 define('DB_HOST', "db-aules.uji.es");
