5ff9d657d9c3fc44161e896859120fb5c877ba86
ATNAislove <al375915@uji.es>
2020-10-17 13:30:17 +0200

    footer

--- partials/footer.php
+++ partials/footer.php
@@ -11,7 +11,7 @@
     Nieves Cubedo Hurtado <a href="mailto:al375915@uji.es">al375915@uji.es</a>
     <br>
     Anna Serisuelo Meneu <a href="mailto:al361883@uji.es">al361883@uji.es</a>
-    <br>
+    <br> <br>
 </footer>
 
 </body>
