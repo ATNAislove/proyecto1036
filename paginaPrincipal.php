488e2fc5b943f7b862290f93dc1648cb55deed85
al361883 <al361883@uji.es>
2020-10-18 01:11:08 +0200

    carousel

--- paginaPrincipal.php
+++ paginaPrincipal.php
@@ -6,6 +6,19 @@
   </head>
 
   <body>
+    <!-- Galeria de imagenes -->
+    <div id="slider">
+        <figure>
+          <!-- Modificar las imagenes a nuestro gusto, la primera es un ejemplo de como poner un link en una imagen-->
+            <a href="https://www.dropbox.com/s/f5g7ruz8hdtwe0v/1.png?raw=1"> <img src="https://www.dropbox.com/s/f5g7ruz8hdtwe0v/1.png?raw=1" />
+            <img src="https://www.dropbox.com/s/iyvj8i3d5aaowiw/2.png?raw=1" />
+            <img src="https://www.dropbox.com/s/4cophdhsag4ew28/3.png?raw=1" />
+            <img src="https://www.dropbox.com/s/c3184gcsdeag8ap/4.png?raw=1" />
+            <img src="https://www.dropbox.com/s/iyvj8i3d5aaowiw/2.png?raw=1" />
+        </figure>
+    </div>
+
+
         <h1>This is a Heading</h1>
         <p>This is a paragraph.</p>
   </body>
