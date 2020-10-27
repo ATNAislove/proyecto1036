a9e9cb16eff69486242d09c6fff7eb48ef143cad
al361883 <al361883@uji.es>
2020-10-27 19:48:16 +0100

    elementos cesta

--- includes/metodos_cesta.php
+++ includes/metodos_cesta.php
@@ -8,10 +8,11 @@ function addCesta() {
     if(!isset($_SESSION['cesta'])){
         $_SESSION['cesta'] = '';
     }
-    if (0 == strlen($_SESSION['cesta']))
+    if (0 == strlen($_SESSION['cesta'])){
         $_SESSION['cesta'] .=  $_GET['product'];
-    else
+    }else{
         $_SESSION['cesta'] .= '#' . $_GET['product'];
+    }
 }
 function borrarDeLaCesta(){
     $pos = $_REQUEST['pos'];
@@ -19,6 +20,20 @@ function borrarDeLaCesta(){
     //print_r($vec,$pos);
     array_splice($vec, intval($pos), 1);
     $_SESSION['cesta'] = implode('#', $vec);
+}
+
+function contarCesta(){
+    if(!isset($_SESSION["cesta"])){
+        return 0;
+    }
+    $vec = explode('#', $_SESSION["cesta"]);
+
+    if($vec[0] == ""){
+        return 0;
+    }else{
+        return count($vec);
+    }
+
 }
 
 ?>
--- partials/menu.php
+++ partials/menu.php
@@ -6,6 +6,8 @@
         
 
         <?php 		
+        include_once(dirname(__FILE__)."/../includes/metodos_cesta.php");
+
         if (!isset($_SESSION['username'])){
                 echo '<li style="float:right"><a href="?action=entrar">Entrar</a></li>';
                 echo '<li style="float:right"><a href="?action=registro">Registrarme</a></li>';
@@ -14,10 +16,13 @@
            if($_SESSION['tipo'] == 'admin'){
                 echo '<li><a href="?action=registrar_producto">Registrar Producto</a></li>';
            }elseif ($_SESSION['tipo'] == 'normal'){
+             if(!isset($_SESSION['cestaCompra'])){
+              $_SESSION['cestaCompra'] = '0';
+             }
                 echo '<li style="float:right"><a href="?action=cesta"><svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-basket2" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path d="M4 10a1 1 0 0 1 2 0v2a1 1 0 0 1-2 0v-2zm3 0a1 1 0 0 1 2 0v2a1 1 0 0 1-2 0v-2zm3 0a1 1 0 1 1 2 0v2a1 1 0 0 1-2 0v-2z"/>
                 <path fill-rule="evenodd" d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-.623l-1.844 6.456a.75.75 0 0 1-.722.544H3.69a.75.75 0 0 1-.722-.544L1.123 8H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6h1.717L5.07 1.243a.5.5 0 0 1 .686-.172zM2.163 8l1.714 6h8.246l1.714-6H2.163z"/>
-              </svg></a></li>';
+              </svg> Cesta: ('; echo contarCesta(); echo'  articulos)</a></li>';
            }
            echo '<li style="float:right"><a href="?action=salir">Salir</a></li>';
         }
