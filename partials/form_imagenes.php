312b78a793ad8663e0a7e36e113a9f512508cd04
al361883 <al361883@uji.es>
2020-11-09 15:21:25 +0100

    imagenes esqueleto

--- partials/form_imagenes.php
+++ partials/form_imagenes.php
@@ -2,16 +2,31 @@
 <html>
     <head>
     <link href="/css/style.css" rel="stylesheet" type="text/css">
-    </head>
 
-        <form action="?action=upload" method="post" enctype="multipart/form-data">
-		<h1>Selecciona	una	imagen: <h2>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+      </head>
 
-        <input type="file" accept="image/*" name="tmp_file" id="upload">
-        <br><br>
-		<input type="submit" class="button" value="Subir" name="submit">
-</form>
+<body>
+     <p>This is the main content. To display a lightbox click <a href="javascript:void(0)" onclick="document.getElementById('light').style.display='block';document.getElementById('fade').style.display='block'">here</a>
+     </p>
+     <div id="light" class="white_content">
+     <button type="button" class="exit" onclick="document.getElementById('light').style.display='none';document.getElementById('fade').style.display='none'">x</button>
 
+     <br> 
+     <h1> Seleccionar imágenes </h1> <br> 
+     <br> 
+     
+     <input type="file" accept="image/*" name="tmp_file" id="upload">
+
+     <canvas id="myCanvas" width="200" height="100"></canvas>
+     
+     <br><br>
+     <input type="submit" class="button" value="Subir" name="submit">
+    
+    </div>
+     <div id="fade" class="black_overlay"></div>
+</body>
 
 </body>
 </html>
