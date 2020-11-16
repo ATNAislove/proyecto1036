ab5606a6d7f5113376d02b9727c4452b2be3a67c
ATNAislove <al375915@uji.es>
2020-11-16 12:05:03 +0100

    metodos cesta sin terminar

--- imports/funciones.js
+++ imports/funciones.js
@@ -22,6 +22,40 @@ function validarFormularioAutentificar(){
         return false;
     }
 }
+//Función añadir elementos a la cesta
+function anyadir(producto){
+    let nodo = document.createElement('tr');
+    if(producto)
+      nodo.textContent = producto;
+    else /*si el contenido es vacio return */
+       nodo.textContent = document.getElementById('producto').value;
+    
+    let nodo2 = document.createElement('button');
+    nodo2.textContent = 'Borrar';
+    nodo.appendChild(nodo2);
+    nodo2.onclick = eliminarNodo.bind(nodo2);
+    //borra igual sin el bind
+    document.getElementById('compra').appendChild(nodo);
+  }
+  //Funcion borrar producto de la cesta
+  function eliminarNodo(){
+    this.parentNode.remove();
+    //hay que borrar de localStorage
+  }
+  //Funcion guardar elementos de la cesta
+  function guardar(){
+    let tabla = document.querySelectorAll('tr');
+    //hay que conseguir que no guarde el contenido del boton
+    tabla = Array.from(tabla).map(n => n.textContent);
+    localStorage.setItem('cesta',JSON.stringify(tabla));
+  }
+  //Muestra los elementos que hay en la cesta
+  (function(){
+    let tabla = JSON.parse(localStorage.getItem('cesta'));
+    if(tabla && tabla.length>0){
+        tabla.forEach(producto => anyadir(producto))
+    }
+  })()
 
 
 //console.log('hola') para depurar por consola, por defecto no es null sino undefined
