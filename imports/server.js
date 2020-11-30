43992bdbe0ff0f7a89203a9c8359c0ac80e5145b
ATNAislove <al375915@uji.es>
2020-11-30 16:45:04 +0100

    ejercicio 2 carrousel funciona correctamente

--- imports/server.js
+++ imports/server.js
@@ -1,6 +1,55 @@
-fetch('./includes/datos.php')
-.then(response => {if(!response.ok){
-    console.log('error'); throw response.statusText;
-    }else return response.json();})
-.then(json => console.log(JSON.stringify(json)))
-.catch(err => console.log('Fetch Error: ', err))
+function items(element){
+    if (element=="" || element==null) return;
+    console.log('aqui llega');
+    console.log(element);
+    console.log(element.produc_id);
+    let nodo = document.createElement('div');
+    nodo.className = 'item';
+    nodo.id=element.produc_id;
+    let nodo2 = document.createElement('img');
+    nodo2.src= element.img;
+    nodo2.width="178";
+    nodo2.height="178";
+    nodo.appendChild(nodo2);
+    let nodo3 = document.createElement('p');
+    
+    nodo3.textContent = element.nombre + ' '+ element.precio + '€';
+    nodo.appendChild(nodo3);
+    let nodo4=document.createElement('button');
+    nodo4.textContent='Añadir a la cesta';
+    nodo4.className='button';
+    
+    nodo4.onclick=anyadir;
+    nodo.appendChild(nodo4);
+    let carrousel = document.getElementById('carr');
+    carrousel.appendChild(nodo);
+}
+//Función añadir elementos a la cesta
+function anyadir(){
+    let producto = this.parentNode.id;
+    let lista = JSON.parse(localStorage.getItem('cesta'));
+    if(producto=="") return;
+    if(lista==null)
+      lista = new Array(producto.toString());
+    else
+        lista.push(producto.toString());
+    //actualizamos localStorage
+    localStorage.setItem('cesta',JSON.stringify(lista));
+  }
+  function borrarCesta(){
+    localStorage.removeItem('cesta');
+  }
+  //para cada producto diccionario llama a la funcion items
+  function auxiliar(json){
+    for(let producto of json){
+        items(producto);
+    }
+  }
+(function(){
+    fetch('./includes/datos.php')
+    .then(response => {if(!response.ok){
+        console.log('error'); throw response.statusText;
+        }else return response.json();})
+    .then(json => auxiliar(json));
+    
+})()
--- includes/datos.php
+++ includes/datos.php
@@ -2,7 +2,7 @@
 include(dirname(__FILE__)."/conector_BD.php");
 global $pdo;
 header('Content-type: application/json');
-$result = $pdo->prepare("SELECT nombre, precio, img FROM producto");
+$result = $pdo->prepare("SELECT produc_id, nombre, precio, img FROM producto");
 $result->execute();
 $datos = $result->fetchAll(PDO::FETCH_ASSOC);
 echo json_encode($datos);
