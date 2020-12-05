56d7ed9bf704fc662844bdb6418ae6fec7429053
ATNAislove <al375915@uji.es>
2020-12-05 23:56:58 +0100

    ejercicio 3 el ejercicio del buscador con sugerencias funciona

--- imports/funcionesPrecios.js
+++ imports/funcionesPrecios.js
@@ -59,6 +59,7 @@ function preciosFiltrados(){
   $max = document.getElementById('max').value;
   //vaciamos el carrousel si es que hay algo
     vaciarCarrousel();
+    //si no se ha introducido algun valor muestra todos los productos
     if ($min=="" || $max=="") {
       todosProductos();
       return;
@@ -84,14 +85,53 @@ function preciosFiltrados(){
         }else return response.json();})
     .then(json => auxiliar(json));
 }
+//funcion que recoje todos los productos sin filtros y llama a otras funciones para que los muestren
+//si el elemento que busco con el buscador no esta en el carrousel porque hemos filtrado
+//habremos llamado a esta funcion para que se muestren todos los productos y asi, con este ultimo then lo muestra en el lado visible del carrousel
 function todosProductos(){
   fetch('./includes/datos.php')
   .then(response => {if(!response.ok){
       console.log('error'); throw response.statusText;
       }else return response.json();})
-  .then(json => auxiliar(json));
+  .then(json => auxiliar(json))
+  .then(x => {if(Prod2ID[document.getElementById('buscador').value]) 
+  document.getElementById(Prod2ID[document.getElementById('buscador').value]).scrollIntoView();}
+  );
 }
+//inserta las opciones en el buscador
+function insertarOpciones(L){
+  //si no le ponemos var dentro de una funcion es global
+  //para hacerla global si no estas en una funcion es con var
+  Prod2ID = {}
+  //para cada producto creamos un elemento opcion con el nombre del producto y
+  // lo almacenamos con su id en Prod2ID, luego lo añadimos a la pagina
+  L.forEach(x => {
+    let n = document.createElement('option');
+    n.value = x.nombre;
+    Prod2ID[x.nombre] = x.produc_id;
+    document.getElementById('prendas').appendChild(n);
+  })
+}
+//muestra en pantalla el producto seleccionado en el buscador
+function mostrarEnPantalla(){
+  let $nombre = document.getElementById('buscador').value;
+  if(!Prod2ID[$nombre]) return;
+  try{
+    document.getElementById(Prod2ID[$nombre]).scrollIntoView();
+  }catch(error){
+    vaciarCarrousel();
+    todosProductos();
+    //document.getElementById(Prod2ID[$nombre]).scrollIntoView();
+  }
+}
+//el primer fetch sirve para insertar las opciones del buscador
+//con el segundo fetch mostramos todos los productos
 (function(){
+fetch('./includes/datos.php')
+.then(response => response.json())
+.then(data => insertarOpciones(data))
+.catch(err => console.log(err))
+
   fetch('./includes/datos.php')
   .then(response => {if(!response.ok){
       console.log('error'); throw response.statusText;
--- partials/form_precios.php
+++ partials/form_precios.php
@@ -7,13 +7,18 @@
         <h1>Filtrar precios</h1>
 
         <form id="formulario">
-        <label for="min">Min:</label>
-        <input type="number" id="min" name="min" required><br><br>
-        <label for="max">Max:</label>
-        <input type="number" id="max" name="max" required><br><br>
-       
-        <input type="submit" name="action" class="button" value="filtrar" onclick=preciosFiltrados()>
+            
+            <label for="buscador">Busca tu prenda:</label>
+            <input list="prendas" name="buscador" id="buscador" onchange=mostrarEnPantalla()>
+            <datalist id="prendas"></datalist> <br><br>
+            <label for="min">Min:</label>
+            <input type="number" id="min" name="min" required>
+            <label for="max">Max:</label>
+            <input type="number" id="max" name="max" required><br><br>
         
+            <input type="submit" name="action" class="button" value="filtrar" onclick=preciosFiltrados()>
+
+                   
         </form> 
 
         <div  id="carr" class="visor">  
